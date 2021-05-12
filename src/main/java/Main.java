c090cdcdca3ba9847deb40aa6f050e1424751b23
Randy Reyna Hernández <randyrh91@gmail.com>
2021-05-13 01:09:48 +0300

    Adicionados los metodos para edad y nombre

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -36,29 +36,63 @@ public class Main {
         }
         return clean_email;
     }
+    public static String cleanAge(String age){
+        String clean_age = "";
+        Pattern pattern = Pattern.compile("(^(120|1[0-1][0-9]|[2-9][0-9]|1[0-9]|[1-9])){1}");
+        Matcher match = pattern.matcher(age);
+        if(match.find()){
+           clean_age = age.substring(match.start(), match.end());
+        }
+        return clean_age;
+    }
+    
+    public static String cleanName(String name){
+        String clean_name = "";
+            Pattern pattern = Pattern.compile("([А-Я]{1}[а-яё]{1,23})?");
+            Matcher match = pattern.matcher(name);
+            while(match.find()){
+               clean_name += name.substring(match.start(), match.end()) + " ";
+            }
+            return clean_name.trim();
+    }
 
     public static void main(String[] args) {
 
         // Patrón para validar el email
         String email = "ra2'n#dy.re3y na@umc&c.cu";
         String regex_email = "^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-        Pattern pattern = Pattern.compile(regex_email);
-        Matcher mather = pattern.matcher(email);
-        if (!mather.find()) {
+        Pattern pattern_email = Pattern.compile(regex_email);
+        Matcher mather_email = pattern_email.matcher(email);
+        if (!mather_email.find()) {
             email = cleanEmail(email);
         }
         
         // Patrón para validar el nombre
         String nombre = "Randy Reyna";
         String regex_name = "^([А-Я]{1}[а-яё]{1,23})?((\\s)[А-Я]{1}[а-яё]{1,23})?((\\s)[А-Я]{1}[а-яё]{1,23})?$";
+        Pattern pattern_name = Pattern.compile(regex_email);
+        Matcher mather_name = pattern_name.matcher(email);
+        if (!mather_name.find()) {
+            email = cleanName(nombre);
+        }
         
         // Patrón para validar la edad
         String edad = "23";
         String regex_age = "^([1-9]|120|1[0-9]|[2-9][0-9])$";
+        Pattern pattern_age = Pattern.compile(regex_email);
+        Matcher mather_age = pattern_age.matcher(email);
+        if (!mather_age.find()) {
+            edad = cleanAge(edad);
+        }
         
         // Patrón para validar el numero
         String numero = "+7 (999) 1111111";
         String regex_numer = "^\\+\\d{1,3}\\s?\\(\\d{3}\\)\\s?\\d{3}([-\\s]\\d{2}){2}$";
+        Pattern pattern_numer = Pattern.compile(regex_email);
+        Matcher mather_numer = pattern_numer.matcher(email);
+        if (!mather_numer.find()) {
+            email = cleanEmail(numero);
+        }
         
         System.out.print(nombre + "|" + edad + "|" + email + "|" + numero);
     }
